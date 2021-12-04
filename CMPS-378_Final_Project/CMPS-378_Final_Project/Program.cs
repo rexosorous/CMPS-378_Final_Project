9303a32020a8bd5592cc3972210b26087b1be160
rexosorous <jzloke@gmail.com>
2021-12-04 12:38:27 -0800

    bot can login but can't respond to commands

--- CMPS-378_Final_Project/CMPS-378_Final_Project/Program.cs
+++ CMPS-378_Final_Project/CMPS-378_Final_Project/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = ConfigureServices(_client, _commands);
+            _client.Log += Log;
+            _commands.Log += Log;
+
+            //await InitCommands();
+
+            string token = System.IO.File.ReadAllText("login.token");
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await _services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+            await Task.Delay(-1);
+        }
+
+        private static IServiceProvider ConfigureServices(DiscordSocketClient _client, CommandService _commands)
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .BuildServiceProvider();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task InitCommands()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+    }
+}
